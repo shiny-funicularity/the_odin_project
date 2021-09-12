a176d84535cfc1d9f19bfd05febd7f58cbbd714a
shiny-funicularity <shiny_funicularity@protonmail.com>
2021-09-12 22:18:30 +0700

    update landing page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ function App() {
   return (
     <div className="app">
       <h1 className="app-title">My Odin Projects</h1>
+      <div className="app-container app-profile">
+        <img src={process.env.PUBLIC_URL + "/profile.jpg"} alt="my profile" />
+        <h2>shiny funicularity</h2>
+        <p>improving my skills <br />with the odin project's assignments</p>
+      </div>
+      <h2 className="app-label">Projects</h2>
+      <div className="app-container">
+        <div className="app-project-item">
+          <div className="app-project-item-left">
+            <button>99 L</button>
+            <p>Odin Recipes</p>
+          </div>
+          <div className="app-project-item-right">
+            <a href="the_odin_project/recipes" target="_blank" rel="noreferrer" >Live Preview</a>
+          </div>
+        </div>
+        <hr className="app-project-item-hr" />
+      </div>
+      
     </div>
   );
 }
