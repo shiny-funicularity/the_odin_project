05ace0caed952f8d60caa1ce5171f8aa2ba01ac4
shiny-funicularity <shiny_funicularity@protonmail.com>
2021-09-14 14:25:33 +0700

    new route

--- src/recipes/App.js
+++ src/recipes/App.js
@@ -1,14 +1,48 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Pla from "./pages/Pla";
+import tubtim from "./pages/tubtim.webp";
+import kapao from "./pages/kapao.webp";
+
+const rootUrl = "/the_odin_project/recipes";
 
 const App = () => {
   return (
-    <div className="app-recipes">
-      <h1 className="app-recipes-title">Odin Recipes</h1>
-      <div className="app-recipes-item">
-        bbb
+    <Router>
+      <div>
+        <Switch>
+          <Route path={rootUrl + "/pla"}>
+            <Pla />
+          </Route>
+          <Route path={rootUrl + "/"}>
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 };
 
 export default App;
+
+function Home() {
+  return (
+    <div className="app-recipes">
+      <h1 className="app-recipes-title">Odin Recipes</h1>
+      <div className="app-recipes-item">
+        <img src={tubtim} alt="Pla Tubtim Neung Manao" />
+        <Link to={rootUrl + "/pla"}>Pla Tubtim Neung Manao</Link>
+      </div>
+      <div className="app-recipes-item">
+        <img src={kapao} alt="Pad Kapao" />
+        <Link to={rootUrl + "/pla"} className="app-recipes-item-link">
+          Pla Tubtim Neung Manao
+        </Link>
+      </div>
+      <Link to={rootUrl + "/pla"} className="app-recipes-item">
+        <img src={kapao} alt="Pad Kapao" />
+        Pla Tubtim Neung Manao
+      </Link>
+    </div>
+  );
+}
---
+++ src/recipes/pages/Pla.js
@@ -0,0 +1,9 @@
+const Pla = () => {
+    return (
+        <div className="app-recipes-pla">
+            Pla Tubtim Neung Manao
+        </div>
+    );
+}
+
+export default Pla;
